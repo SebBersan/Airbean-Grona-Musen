14ffca64ad435cdd1fa4f7730953b1f6e95896d9
Jakob Malmgren <Jakob.Malmgren1987@gmail.com>
2025-02-24 10:39:43 +0100

    created the statusModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 // import { useState } from 'react'
 
-import './App.css'
+import "./App.css";
+import StatusModal from "./components/StatusModal";
 
 function App() {
-
-
   return (
     <>
-
+      <StatusModal />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
